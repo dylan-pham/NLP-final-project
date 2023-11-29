0de60d45daa58e393c1b9808a46d1fb02b19d4a0
Dylan Pham <43.dpham@gmail.com>
2023-11-29 14:15:12 -0500

    LR progress

--- utils.py
+++ utils.py
@@ -47,3 +47,64 @@ def split_data(data, test_size=0.2):
     X, y = data
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size)
     return ((X_train, y_train), (X_test, y_test))
+
+def create_index(all_train_data_X: list) -> list:
+    """
+    Given the training data, create a list of all the words in the training data.
+    Args:
+        all_train_data_X: a list of all the training data in the format [[word1, word2, ...], ...]
+    Returns:
+        vocab: a list of all the unique words in the training data
+    """
+    # figure out what our vocab is and what words correspond to what indices
+    #([[]], [])
+
+    vocab = set()
+    documents = all_train_data_X[0]
+    for document in documents:
+        for word in document:
+            if word not in vocab:
+                vocab.add(word)
+    return list(vocab)
+
+def featurize(vocab: list, data_to_be_featurized_X: list, binary: bool = False, verbose: bool = False) -> list:
+    """
+    Create vectorized BoW representations of the given data.
+    Args:
+        vocab: a list of words in the vocabulary
+        data_to_be_featurized_X: a list of data to be featurized in the format [[word1, word2, ...], ...]
+        binary: whether or not to use binary features
+        verbose: boolean for whether or not to print out progress
+    Returns:
+        a list of sparse vector representations of the data in the format [[count1, count2, ...], ...]
+    """
+    # using a Counter is essential to having this not take forever
+    vectorized_data = []
+
+    documents = data_to_be_featurized_X[0]
+    for sample in documents:
+        vector = []
+        sample_counter = Counter(sample)
+        for word in vocab:
+            if binary:
+                if word in sample_counter:
+                    vector.append(1)
+                    if verbose:
+                        print("Added 1 to current vector.")
+                else:
+                    vector.append(0)
+                    if verbose:
+                        print("Added 0 to vectorized data.")
+            else:
+                vector.append(sample_counter[word])
+                if verbose:
+                    print(f"{word} was added with value {sample_counter[word]}")
+
+        vectorized_data.append(vector)
+        if verbose:
+            print(f"Adding current vector: {vector} to vectorized data.")
+
+    if verbose:
+        print("Vectorization completed.")
+
+    return vectorized_data
