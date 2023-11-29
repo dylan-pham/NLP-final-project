1229df5668e8d6a7a7c788a31696717278980517
Dylan Pham <43.dpham@gmail.com>
2023-11-29 12:34:17 -0500

    init

---
+++ utils.py
@@ -0,0 +1,33 @@
+import nltk 
+
+def generate_tuples_from_file(training_file_path: str) -> list:
+    """
+    Generates data from file formated like:
+
+    tokenized text from file: [[word1, word2, ...], [word1, word2, ...], ...]
+    labels: [0, 1, 0, 1, ...]
+    
+    Parameters:
+        training_file_path - str path to file to read in
+    Return:
+        a list of lists of tokens and a list of int labels
+    """
+    # PROVIDED
+    f = open(training_file_path, "r", encoding="utf8")
+    X = []
+    y = []
+    for review in f:
+        if len(review.strip()) == 0:
+            continue
+        dataInReview = review.strip().split("\t")
+        if len(dataInReview) != 3:
+            continue
+        else:
+            t = tuple(dataInReview)
+            if (not t[2] == '0') and (not t[2] == '1'):
+                print("WARNING")
+                continue
+            X.append(nltk.word_tokenize(t[1]))
+            y.append(int(t[2]))
+    f.close()  
+    return (X, y)
